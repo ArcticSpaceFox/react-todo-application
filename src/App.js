b173392df6e475da326c6a450c11ac2cf624d2e3
Sm03leBr00t <Sm03leBr00t@protonmail.com>
2019-10-12 18:09:14 +0200

    basic working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import { TodoProvider } from "./TodoContext";
+
+import Navbar from "./components/navabar";
+import TodoList from "./components/todolist";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TodoProvider>
+      <Navbar/>
+      <TodoList/>
+    </TodoProvider>
   );
 }
 
---
+++ src/components/todo.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default function Todo(props) {
+  return (
+    <div className="card">
+      <div className="card-content">
+        <div className="level">
+          <div className="level-left">
+            <div className="level-item">
+              <p className={`title ${props.todo.done ? "has-text-primary" : ""}`}>{props.todo.value}</p>
+            </div>
+          </div>
+          <div className="level-right">
+            <div className="level-item buttons">
+              <button className={`button has-text-weight-bold ${props.todo.done ? "is-warning" : "is-primary"}`}>{props.todo.done ? "Undo" : "Done"}</button>
+              <button className="button is-danger has-text-weight-bold">Delete</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
